37541ff72446b1671d9ff3472ec625d19a712de1
daniel millson <danielmillson923@gmail.com>
2022-12-17 13:27:33 -0500

    Day 10 Challenge Completed

--- Day9/main.go
+++ Day9/main.go
@@ -24,10 +24,12 @@ func absDiff(a, b int) int {
 	return int(math.Abs(float64(a - b)))
 }
 
-func handleMove(v []string, head, tail *Coord, set *mapset.Set[Coord]) {
+func handleMove(v []string, rope []*Coord, set *mapset.Set[Coord]) {
 	moveCount, _ := strconv.Atoi(v[1])
 
 	for i := 0; i < moveCount; i++ {
+		head := rope[0]
+
 		// Move Head
 		switch v[0] {
 		case "U":
@@ -39,26 +41,39 @@ func handleMove(v []string, head, tail *Coord, set *mapset.Set[Coord]) {
 		case "L":
 			(*head).x -= 1
 		}
-		// Adjust Tail
-		if dist(head, tail) > 1 {
-			if absDiff(head.x, tail.x) > 1 {
-				if head.y != tail.y {
-					(*tail).y = head.y
+
+		for knotIndex := 0; knotIndex < len(rope)-1; knotIndex++ {
+			current := rope[knotIndex]
+			next := rope[knotIndex+1]
+
+			// Adjust Next
+			if dist(current, next) > 1 {
+				if absDiff(current.x, next.x) > 1 {
+					if current.y != next.y {
+						(*next).y += ((current.y - next.y) / absDiff(current.y, next.y))
+					}
+					(*next).x += ((current.x - next.x) / absDiff(current.x, next.x))
+				} else if absDiff(current.y, next.y) > 1 {
+					if current.x != next.x {
+						(*next).x += ((current.x - next.x) / absDiff(current.x, next.x))
+					}
+					(*next).y += ((current.y - next.y) / absDiff(current.y, next.y))
 				}
-				(*tail).x += ((head.x - tail.x) / absDiff(head.x, tail.x))
-			} else if absDiff(head.y, tail.y) > 1 {
-				if head.x != tail.x {
-					(*tail).x = head.x
-				}
-				(*tail).y += ((head.y - tail.y) / absDiff(head.y, tail.y))
 			}
 		}
 
-		// fmt.Printf("head: %+v\n", head)
-		// fmt.Printf("tail: %+v\n", tail)
+		// Print rope after move sequence
+		grid := make([][]string, 10) // 60
+		for i := 0; i < len(grid); i++ {
+			row := []string{}
+			for j := 0; j < 10; j++ {
+				row = append(row, ".")
+			}
+			grid[i] = row
+		}
 
 		// Add to set
-		(*set).Add(*tail)
+		(*set).Add(*rope[len(rope)-1])
 	}
 }
 
@@ -73,19 +88,21 @@ func main() {
 	defer file.Close()
 
 	visitedCoords := mapset.NewSet[Coord]()
-	headCoord := Coord{0, 0}
-	tailCoord := Coord{0, 0}
+	rope := make([]*Coord, 10)
 
-	visitedCoords.Add(tailCoord)
+	for i := range rope {
+		rope[i] = &Coord{0, 0}
+	}
+
+	visitedCoords.Add(*rope[len(rope)-1])
 
 	// Read from file and process moves
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		str := scanner.Text()
 		v := strings.Split(str, " ")
-		// fmt.Printf("v: %v\n", v)
 
-		handleMove(v, &headCoord, &tailCoord, &visitedCoords)
+		handleMove(v, rope, &visitedCoords)
 	}
 
 	fmt.Printf("visitedCoords.Cardinality(): %v\n", visitedCoords.Cardinality())
